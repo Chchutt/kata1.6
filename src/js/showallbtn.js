d7ff73d5db949385b06a79522e6e3559040b1a54
Chchutt <shabalinart@bk.ru>
2022-05-27 18:36:40 +0300

    1.5

--- src/js/showallbtn.js
+++ src/js/showallbtn.js
@@ -1,3 +1,21 @@
 const showAll = document.querySelector(".brands-menu__show-all");
-
-
+const showAlltext = document.querySelector(".brands-text__show-all");
+const brand9 = document.querySelector(".brand-9");
+const brand10 = document.querySelector(".brand-10");
+const brand11 = document.querySelector(".brand-11");
+let statusMenuOpenedBrand = true;
+showAll.addEventListener("click", function (){
+  if (statusMenuOpenedBrand) {
+    brand9.style.display = "inline";
+    brand10.style.display = "inline";
+    brand11.style.display = "inline";
+    showAlltext.innerHTML = "<p>Скрыть</p>"
+    statusMenuOpenedBrand = false;
+  } else {
+    brand9.style.display = "none";
+    brand10.style.display = "none";
+    brand11.style.display = "none";
+    showAlltext.innerHTML = "<p>Показать все</p>"
+    statusMenuOpenedBrand = true;
+  }
+})
