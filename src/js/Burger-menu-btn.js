0ed192a672b8f91531cd660f4167a5cfbbffcb17
Chchutt <shabalinart@bk.ru>
2022-06-06 16:35:00 +0300

    1.6 fin

--- src/js/Burger-menu-btn.js
+++ src/js/Burger-menu-btn.js
@@ -1,23 +1,29 @@
 let burgerBtnOpen = document.querySelector(".header__elem1");
 let burgerMenu = document.querySelector(".mobile-menu-main-container");
 let burgerBtnClose = document.querySelector(".menu-back");
-let burgerBtnActive = document.querySelector(".active")
-let statusBurgerMenu = true;
-burgerBtnOpen.addEventListener("click", function (evt){
-  evt.preventDefault();
-  burgerMenu.classList.add("active");
-})
-burgerBtnClose.addEventListener("click", function (evt){
-  evt.preventDefault();
-  burgerMenu.classList.remove("active");
-})
+let modalBg = document.querySelector(".popup-black")
 
-window.addEventListener("mouseup", function(e){
-if (burgerMenu !== e.target) // если клик был не по нашему полю
-{
-  if (burgerMenu.classList.contains("active"))
-  {
+document.addEventListener('click', function(evt) {
+  evt.preventDefault(burgerBtnClose);
+  if (window.innerWidth < 1440 && evt.target.closest(".header__elem1")) {
+    burgerMenu.classList.toggle("active");
+  } else if (window.innerWidth < 1440 && (!evt.target.closest(".mobile-menu-main-container"))
+    || (evt.target.closest(".menu-back"))) {
     burgerMenu.classList.remove("active");
   }
-}
 });
+// burgerBtnOpen.addEventListener("click", function (evt){
+//   evt.preventDefault();
+//   burgerMenu.classList.add("active");
+// })
+// burgerBtnClose.addEventListener("click", function (evt){
+//   evt.preventDefault();
+//   burgerMenu.classList.remove("active");
+// })
+//
+// window.addEventListener("click", function (event) {
+//   if (event.target !== burgerMenu) {
+//     burgerMenu.classList.remove("active");
+//   }
+// });
+
